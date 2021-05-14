b6014fbaba7049e428c35c4f5132f623fb931827
MinaSamir <menasamercap@gmail.com>
2021-05-14 21:05:02 +0200

    first commit

---
+++ app/src/main/java/com/mmd/bottomnavcircle/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.mmd.bottomnavcircle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNav.background = null
+        bottomNav.menu.getItem(2).isEnabled = false
+    }
+}
